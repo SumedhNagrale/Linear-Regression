4dcfa8a6a4af32c9eacef13b0d318c815da1efdb
SumedhNagrale <51673422+SumedhNagrale@users.noreply.github.com>
2019-06-11 00:08:57 -0400

    Update DatasetInitialise.m

--- DatasetInitialise.m
+++ DatasetInitialise.m
@@ -5,10 +5,11 @@ if(AlgorithmTestingOn == 'fludata')
 %And converts it into a table.
 SheetNumber = 1;
 [CH,CV,ActualData] = xlsread('fluML.xlsx',SheetNumber);
+%arranging the titles
+% Data cleaning
 rowHeadings = {'Student','Vaccin','HndWshQual','HndWshFreq','SocialDist','NotTchFace','RespEtiq','PrsnlDist','HandSanit','Risk','Complic','Barriers','Inefficacy','KnowlTrans','KnowlMgmt','Sick','Flu','Gender'};
 Grad = cell2struct(ActualData, rowHeadings, 2);
 Grad(1) = [];
-
 FluDataset = struct2table(Grad);
 FluDataset(any(isnan(FluDataset.Risk),2) == 1,:) = [];
 FluDataset(any(isnan(FluDataset.KnowlTrans),2) == 1,:) = [];
@@ -18,7 +19,7 @@ FluDataset(index0,:) = [];
 % index1 = find(FluDataset.Risk== max(FluDataset.Risk(:)))
 % FluDataset(index1,:) = [];
 clearvars -except FluDataset;
-%Scaling::
+% Scaling::
 % % FluDataset.Risk  = (((3*(FluDataset.Risk - min(FluDataset.Risk))/(max(FluDataset.Risk)-min(FluDataset.Risk))))- 1.5);
 % FluDataset.KnowlTrans = (((2.9*(FluDataset.KnowlTrans - min(FluDataset.KnowlTrans))/(max(FluDataset.KnowlTrans)-min(FluDataset.KnowlTrans))))- 1.45);
 % FluDataset.RespEtiq  = (((2.9*(FluDataset.RespEtiq - min(FluDataset.RespEtiq))/(max(FluDataset.RespEtiq)-min(FluDataset.RespEtiq))))- 1.45);
@@ -26,7 +27,7 @@ FluDataset.Risk  = ((((FluDataset.Risk - min(FluDataset.Risk))/(max(FluDataset.R
 FluDataset.KnowlTrans = ((((FluDataset.KnowlTrans - min(FluDataset.KnowlTrans))/(max(FluDataset.KnowlTrans)-min(FluDataset.KnowlTrans)))));
 FluDataset.RespEtiq  = ((((FluDataset.RespEtiq - min(FluDataset.RespEtiq))/(max(FluDataset.RespEtiq)-min(FluDataset.RespEtiq)))));
 else
-    
+    % Add a different Data set
 end    
 end
 
